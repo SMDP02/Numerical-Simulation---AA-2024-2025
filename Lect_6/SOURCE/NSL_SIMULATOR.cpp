c140151e69ac55ec4d7e0ad3547cf68fc2db219e
Simone Maria Di Paolo <162431276+SMDP02@users.noreply.github.com>
2026-02-21 19:35:28 +0100

    Add files via upload

---
+++ Lect_6/SOURCE/NSL_SIMULATOR.cpp
@@ -0,0 +1,67 @@
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
+
+#include <iostream>
+#include "system.h"
+
+using namespace std;
+
+int main (int argc, char *argv[]){
+
+  System SYS;
+  SYS.initialize();
+  SYS.initialize_properties();
+  SYS.block_reset(0);
+
+  int N_eq = 10000; 
+
+  int n_stepT = 20;
+  double T = 2.0;
+  double T_min = 0.5;
+  double dT = (T-T_min)/n_stepT;
+
+  for (int n=0; n<=n_stepT; n++){
+
+    SYS.set_temp(T);
+    SYS.reset_averages();
+
+    // Equilibration
+    for (int m = 0; m < N_eq; m++) {
+      SYS.step();
+    }
+
+    for(int i=0; i < SYS.get_nbl(); i++){ //loop over blocks
+      SYS.block_reset(i+1); 
+      for(int j=0; j < SYS.get_nsteps(); j++){ //loop over steps in a block
+        SYS.step();
+        SYS.measure();
+      }
+      SYS.averages(i+1);
+      SYS.block_reset(i+1);
+    }
+    SYS.finalize();
+
+    T -= dT;
+
+  }
+
+  return 0;
+
+}
+
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
